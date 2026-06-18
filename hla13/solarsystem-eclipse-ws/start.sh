6e9a5484cd77daf5230add35031a3d6a2b5db4a8
harlan3 <harlan3@hotmail.com>
2026-06-18 18:24:13 -0400

    Updated start scripts

--- hla13/solarsystem-eclipse-ws/start.sh
+++ hla13/solarsystem-eclipse-ws/start.sh
@@ -4,7 +4,7 @@ export JAVA_CMD=/usr/local/jdk-22/bin/java
 export JAVA_HOME=/usr/local/jdk-22
 export RTI_LIB_HOME=/rtis_D39/lib/x86_64_g++-11.2
 
-export RTI_RID_FILE=/rtis_D39/Sample.rid
+export RTI_RID_FILE=/CDR/Data/default.rid
 
 # VM options and JavaFX modules
 export VM_OPTIONS="-Djava.library.path=$RTI_LIB_HOME"
