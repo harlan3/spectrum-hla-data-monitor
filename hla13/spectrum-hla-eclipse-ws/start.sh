e92b0835c67b02fea0e17d84664c63ac7b6ba2c1
harlan3 <harlan3@hotmail.com>
2026-06-18 17:12:45 -0400

    Added linux start scripts

---
+++ hla13/spectrum-hla-eclipse-ws/start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export JAVA_CMD=/usr/local/jdk-22/bin/java
+export JAVA_HOME=/usr/local/jdk-22
+export RTI_LIB_HOME=/rtis_D39/lib/x86_64_g++-11.2
+
+export RTI_RID_FILE=/rtis_D39/Sample.rid
+
+# VM options and JavaFX modules
+export VM_OPTIONS="-Djava.library.path=$RTI_LIB_HOME"
+
+export PATH=$JAVA_HOME/bin:$RTI_LIB_HOME
+
+# Add the required jars to classpath
+export CP="./dist/spectrum-hla-data-monitor.jar:./lib/outline.jar:./lib/gson-1.7.jar:./lib/container_classes.jar:./lib/encoding_classes.jar:/rtis_D39/lib/x86_64_g++-11.2/rtis13.jar"
+
+$JAVA_CMD $VM_OPTIONS -cp $CP orbisoftware.hla_tools.spectrum_hla_monitor.MainApplication "$@"
