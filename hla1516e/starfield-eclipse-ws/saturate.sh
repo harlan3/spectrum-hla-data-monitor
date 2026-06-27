ce84194b20c7139c7789b1f5668d29d7aaaaac3a
harlan3 <harlan3@hotmail.com>
2026-06-26 21:38:28 -0400

    Updated scripts for starfield

--- hla1516e/starfield-eclipse-ws/saturate.sh
+++ hla1516e/starfield-eclipse-ws/saturate.sh
@@ -12,9 +12,9 @@ export VM_OPTIONS="-Djava.library.path=$RTI_LIB_HOME"
 export PATH=$JAVA_HOME/bin:$RTI_LIB_HOME
 
 # Add the required jars to classpath
-export CP="./dist/solar_system.jar:./lib/container_classes.jar:./lib/encoding_classes.jar:/rtis_D39/lib/x86_64_g++-11.2/rtis1516e.jar"
+export CP="./dist/star_field.jar:./lib/container_classes.jar:./lib/encoding_classes.jar:/rtis_D39/lib/x86_64_g++-11.2/rtis1516e.jar"
 
 for i in {1..5}; do
-	$JAVA_CMD $VM_OPTIONS -cp $CP orbisoftware.hlatools.spectrumhlamonitor.solarsystemdemo.MainApplication "$@$i" &
+	$JAVA_CMD $VM_OPTIONS -cp $CP orbisoftware.hlatools.spectrumhlamonitor.starfielddemo.MainApplication "$@$i" &
 	#echo "$@$i" &
 done
